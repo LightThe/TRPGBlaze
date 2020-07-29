8a373efa432d856a48b4cd44b06e4b61b45e4488
Théo Gallo <gallotheo@icloud.com>
2020-07-29 17:11:46 -0300

    First commit

---
+++ Atributo.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+public class Atributo{
+    public string Nome { get; set; }
+    public int Valor { get; set; }
+    public int Mod { get; set; }
+
+    Dictionary<int, int> ModLookup = new Dictionary<int, int>();
+
+
+    public void CalculaModificador(){
+        
+        return ;
+    }
+}
